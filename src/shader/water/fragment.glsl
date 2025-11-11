9d46f11fab148c1eea86a22db3b86dae8c644246
LeomharT <leo.liao.lzy@gmail.com>
2025-11-11 09:26:42 +0800

    feat: ok edge has apparire

--- src/shader/water/fragment.glsl
+++ src/shader/water/fragment.glsl
@@ -19,8 +19,12 @@ void main(){
     if(noiseColor.r < 0.5) discard;
     if(uv.x < 0.01 || uv.x > 0.61) discard;
     if(uv.y < 0.01 || uv.y > 0.61) discard;
- 
-    color = noiseColor.rgb;
+
+    vec3 edgeColor = 1.0 - vec3(noiseColor.r);
+    edgeColor *= 50.0;
+    edgeColor = fract(edgeColor);
+
+    color = edgeColor;
 
     gl_FragColor = vec4(color, 1.0);
 }
